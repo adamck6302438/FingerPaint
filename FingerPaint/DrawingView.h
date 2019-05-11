4bd97ac4e7b9e15fc3bbf0aa4fb7c1b1d3636cce
Ray Kang <adamck6302438@gmail.com>
2019-05-11 12:21:03 -0400

    first commit

---
+++ FingerPaint/DrawingView.h
@@ -0,0 +1,17 @@
+//
+//  DrawingView.h
+//  FingerPaint
+//
+//  Created by Frances ZiyiFan on 5/10/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DrawingView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FingerPaint/DrawingView.m
@@ -0,0 +1,21 @@
+//
+//  DrawingView.m
+//  FingerPaint
+//
+//  Created by Frances ZiyiFan on 5/10/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import "DrawingView.h"
+
+@implementation DrawingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
